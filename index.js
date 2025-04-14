d1aafb9f62e20e94ea81454fc355d40aff7274d1
AngelaZQ1 <zheng.ang@northeastern.edu>
2025-04-14 11:45:38 -0400

    changes

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ import cors from 'cors';
 import "dotenv/config";
 import express from 'express';
 import session from "express-session";
+import mongoose from 'mongoose';
 import AssignmentRoutes from './Kambaz/Assignments/routes.js';
 import CourseRoutes from './Kambaz/Courses/routes.js';
 import EnrollmentRoutes from './Kambaz/Enrollments/routes.js';
 import ModuleRoutes from "./Kambaz/Modules/routes.js";
 import UserRoutes from './Kambaz/Users/routes.js';
 import Lab5 from './Lab5/index.js';
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+mongoose.connect(CONNECTION_STRING);
 
 
 const app = express()
