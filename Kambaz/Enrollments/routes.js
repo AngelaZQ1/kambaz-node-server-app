856da0f3f57d013854532d04bfd6828c4aba6e3e
AngelaZQ1 <zheng.ang@northeastern.edu>
2025-04-16 12:37:50 -0400

    working enrollments

--- Kambaz/Enrollments/routes.js
+++ Kambaz/Enrollments/routes.js
@@ -2,10 +2,6 @@ import * as dao from "./dao.js";
 
 
 export default function EnrollmentRoutes(app) {
-    app.get("/api/enrollments", (req, res) => {
-        const enrollments = dao.getEnrollments();
-        res.send(enrollments);
-    });
     app.post("/api/enrollments/userId/:userId/courseId/:courseId", (req, res) => {
         const { userId, courseId } = req.params;
         const status = dao.enrollUserInCourse(userId, courseId);
